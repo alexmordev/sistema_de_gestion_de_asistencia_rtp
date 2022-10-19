5c3517979f2fd0c5270eab6f235516c4fbb0bac0
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-10-19 16:36:56 -0500

    19/10/2022

--- services/incapacidad.service.js
+++ services/incapacidad.service.js
@@ -45,20 +45,20 @@ class IncapacidadService {
 
   }
   // idPeriodo//
-  async findOnePeriodo(query) {
+  async findOnePeriodo(req) {
 
     const options = {
       attributes: ['id_periodos','per_fecha_inicio','per_fecha_final'],
       where: {}
     };
-    (query.per_numero)
-      ?  options.where.per_numero = query.per_numero
+    (req.per_numero)
+      ?  options.where.per_numero = req.per_numero
       :  null;
-    (query.per_aho)
-        ?  options.where.per_aho = query.per_aho
+    (req.per_aho)
+        ?  options.where.per_aho = req.per_aho
         :  null;
-    (query.per_tipo)
-      ?  options.where.per_tipo = query.per_tipo
+    (req.per_tipo)
+      ?  options.where.per_tipo = req.per_tipo
       :  null;
   
     const periodo = await models.Periodo.findOne(options);
@@ -76,47 +76,23 @@ class IncapacidadService {
     const datosArray = [];
     idPeriodo.forEach(datos => {
 
-      console.log(PFI,PFF);
+      // console.log(PFI,PFF);
       // console.log(datos.fechaInicio);
       const FII = new Date(datos.fechaInicio);//fechaInicioIncapacidad
       const FFI = new Date( datos.fechaFinal);//fechaFinalIncapacidad
-      console.log({FII:FII,FFI:FFI});
+      // console.log({FII:FII,FFI:FFI});
 
     if( (FII.getTime() >= PFI.getTime()) && ( FII.getTime() <= PFF.getTime() ) ){
-      datosArray.push({msg: 'Entro al uno'})
+      datosArray.push({msg: 'Entro al 1', Unidades:datos.unidades})
+      
     }else if( ( FFI.getTime() <= PFF.getTime()) && ( FFI.getTime() >= PFI.getTime() ) ){
-      datosArray.push({msg: 'Entro al 2'})
+      datosArray.push({msg: 'Entro al 2',  Unidades:datos.unidades})
     }else{
-      datosArray.push({msg: 'Ninguno'})
+      datosArray.push({msg: 'No entran en el rango de fechas'})
     }
 
-
-      // console.log(datos);
     });
     console.log({datos: datosArray});
-
-    // console.log(idPeriodo[0].dataValues.fechaInicio);
-    // console.log(idPeriodo);
-    // console.log({ooooo: periodo.dataValues.per_fecha_inicio});
-
-    // console.log({mostratDatos: idPeriodo});
-
-    // return({mostratDatos: periodo.dataValues});
-
-    // const FII = new Date(idPeriodo[0].dataValues.fechaInicio);//fechaInicioIncapacidad
-    // const FFI = new Date( idPeriodo[0].dataValues.fechaFinal);//fechaFinalIncapacidad
-
-    // // (FII.getTime() >= PFI.getTime()) && ( FII.getTime() <= PFF.getTime() )1
-    // // ( FFI.getTime() <= PFF.getTime()) && ( FFI.getTime() >= PFI.getTime() )2
-
-    // if( (FII.getTime() >= PFI.getTime()) && ( FII.getTime() <= PFF.getTime() ) ){
-    //  return 'true 1'
-    // }else if( ( FFI.getTime() <= PFF.getTime()) && ( FFI.getTime() >= PFI.getTime() ) ){
-    //   return ({Datos:idPeriodo})
-    // }else{
-    //   throw new Error
-    // }
-
 
   }
   
