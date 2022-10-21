368a493b84170fc94a2ecece09abaa915965306c
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-10-21 15:50:34 -0500

    21/10/2022 03:50

--- services/reporteGeneral.service.js
+++ services/reporteGeneral.service.js
@@ -10,47 +10,28 @@ class ReporteGeneralService {
 
     const incapacidad = await models.Incapacidad.findAll({
         attributes: ['id_altas_SGA','umf','clave_seguro', 'fecha_expedicion'],
-        // where: { id_altas_SGA: 'id_altas_SGA  ' },
+        
         include: [
           {
             as: 'altas_sga', 
             model: models.AltasSGA, 
-            attributes:['id','id_trabajador','fecha_inicio', 'fecha_final'],
+            attributes:['id','fecha_inicio', 'fecha_final'],
+            where: { id_trabajador: query.id_trabajador },
+            include: [
+              { 
+                as: 'trabajador_vista', 
+                model: Trabajador,
+                attributes:['trabCredencial','moduloClave','tipoTrabProc','nombreCompleto','trab_no_afiliacion','trab_rfc','trab_curp'],
+              },
+            ]
           },
         ],
     })
-       const trabajador = await models.AltasSGA.findAll({
-          attributes: ['id_trabajador','fecha_inicio', 'fecha_final'],
-          where: { id_trabajador: query.id_trabajador },
-          include: [
-            { as: 'trabajador_vista', 
-              model: Trabajador,
-              attributes:['trabCredencial','moduloClave','tipoTrabProc','nombreCompleto','trab_no_afiliacion','trab_rfc','trab_curp'],
-            },
-          ]
-      })
-    const registros =  trabajador[0]._previousDataValues;
+
     const incapacidad1 =  incapacidad;
-    console.log(registros);
-    return ({1: registros, 2: incapacidad1 });
+    // console.log(registros);
+    return ({1: incapacidad1 });
   }
-
-//   const altasId = await models.AltasSGA.findAll({
-//     attributes: ['id_trabajador','fecha_inicio', 'fecha_final'],
-//     where: { id_trabajador: query.id_trabajador },
-//     include: [
-//       { as: 'trabajador_vista', 
-//         model: Trabajador,
-//         attributes:['trabCredencial','moduloClave','tipoTrabProc','nombreCompleto','trab_no_afiliacion','trab_rfc','trab_curp'],
-//       },
-//     ]
-// })
-
-// const registros =  altasId ;
-// return registros;
-// }
-  
-  
 
   async update(id, changes) {
     const ausencia = await this.findOne(id);
