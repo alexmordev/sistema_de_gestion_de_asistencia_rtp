cc57b98b47192feef679436b10e4cd905da375c4
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-11-01 09:58:20 -0600

    Cambios SerchMan

--- services/sancion.service.js
+++ services/sancion.service.js
@@ -1,13 +1,29 @@
 const boom = require('@hapi/boom');
 const {models} = require('../libs/sequelize');
-
+const  FormatingDate  =  require( '../class/FormatingDate' );
 
 class SancionService {
   constructor() {}
 
   async create(data) {
-    const newUser =  await models.AltasSGA.create( data ) //crear
-    return newUser;
+    let { dateFormatedInit, dateFormatedEnd } = FormatingDate.dateFormated( data.fechaInicio, data.fechaFinal )
+    
+    const newSancion =  await models.AltasSGA.create({
+      ...data,
+      fechaInicio : dateFormatedInit,
+      fechaFinal : dateFormatedEnd
+
+    } )
+    const newSancionDatesFormated = FormatingDate.dateFormated( newSancion.fechaInicio, newSancion.fechaFinal );
+    dateFormatedInit = newSancionDatesFormated.dateFormatedInit
+    dateFormatedEnd = newSancionDatesFormated.dateFormatedEnd
+
+    const newSancionDateModify = {
+      ...newSancion.dataValues,
+      fechaInicio : dateFormatedInit,
+      fechaFinal : dateFormatedEnd,
+    }
+    return newSancionDateModify;
   }
 
   async find() {
@@ -29,27 +45,64 @@ class SancionService {
         include:['trabajador_vista', 'trab_periodos','catalogo_conceptos'],
         where:
         {
-          id_concepto:3
+          id_concepto:5
         }
       }
-    );// buscar con id
+    );
     if(!res){
       boom.notFound('Category Not Found');
     }
-    return res;
-  }
+    let { dateFormatedInit, dateFormatedEnd } = FormatingDate.dateFormated( res.dataValues.fechaInicio, res.dataValues.fechaFinal )
+    
+    const resPeriodoDateFormated = FormatingDate.dateFormated(  res.dataValues.trab_periodos.perFechaInicio,  res.dataValues.trab_periodos.perFechaFinal );
+    const formatedInitDatePeriodo = resPeriodoDateFormated.dateFormatedInit
+    const formatedEndDatePeriodo = resPeriodoDateFormated.dateFormatedEnd
+
+    const resDateModify = {
+      ...res.dataValues,
+      fechaInicio : dateFormatedInit,
+      fechaFinal : dateFormatedEnd,
+      trab_periodos:{
+        ...res.dataValues.trab_periodos.dataValues,
+        perFechaInicio: formatedInitDatePeriodo,
+        perFechaFinal: formatedEndDatePeriodo,
+      }
+    }
+    return resDateModify;
+  };
 
   async update(id, changes) {
-    const user = await this.findOne(id);
-    const res = await user.update(changes);
-    return res;
-  }
+    let { dateFormatedInit, dateFormatedEnd } = FormatingDate.dateFormated( changes.fechaInicio, changes.fechaFinal );
+    
+    const sancion  =  await models.AltasSGA.findByPk(id,);
+    if(!sancion){
+      boom.notFound('Sancion Not Found');
+    }
+    const res = await sancion.update({
+      ...changes,
+      fechaInicio : dateFormatedInit,
+      fechaFinal : dateFormatedEnd
+    });
+  
 
+    const resDateFormated = FormatingDate.dateFormated( res.fechaInicio, res.fechaFinal );
+    dateFormatedInit = resDateFormated.dateFormatedInit
+    dateFormatedEnd = resDateFormated.dateFormatedEnd
+
+    const resDateModify = {
+      ...res.dataValues,
+      fechaInicio : dateFormatedInit,
+      fechaFinal : dateFormatedEnd,
+    }
+    return resDateModify;
+  }
   async delete(id) {
-    const user = await this.findOne(id);
-    await user.destroy()
+    const sancion  =  await models.AltasSGA.findByPk(id);
+    if(!sancion){
+      boom.notFound('Sancion Not Found');
+    }
+    await sancion.destroy()
     return {id};
   }
 }
-
 module.exports = SancionService;
