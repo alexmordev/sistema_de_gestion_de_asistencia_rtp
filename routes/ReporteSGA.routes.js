7dfa9b60984f5ce20b8a798b2f27838ee4312e33
Mario Jimenez030399 <69279408+MarioJime@users.noreply.github.com>
2022-12-20 09:38:12 -0600

    Cambio forma de recibir fechas Incawin

--- routes/ReporteSGA.routes.js
+++ routes/ReporteSGA.routes.js
@@ -8,7 +8,7 @@ const service = new (ReporteSGAService);
 router.get('/', async (req, res, next) => {
     validatorHandler(getReproteSGASchema, 'query')
     try {
-        const data = await service.reporteSGA( req.query );
+        const data = await service.reporteSGA(req.query);
         res.json( data );
     } catch (error) {
         next(error);
